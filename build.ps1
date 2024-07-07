e6ce9c7c8f81435832af03577403c2cbbdee3e94
chirp24 <andrewuwuemail2@gmail.com>
2024-07-07 06:33:15 -0400

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,9 @@
+# thank you b7
+$Name="extension"
+echo "Building for Linux..."
+$env:GOOS="linux"; go build -o bin/${Name}-linux .
+echo "Building for Mac..."
+$env:GOOS="darwin"; go build -o bin/${Name}-mac .
+echo "Building for Windows..."
+$env:GOOS="windows"; go build -o bin/${Name}-win.exe -ldflags="-H=windowsgui" .
+echo "Build complete."
